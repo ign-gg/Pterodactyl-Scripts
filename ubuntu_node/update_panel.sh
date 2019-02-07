0e0a06f77ae8c2d27bed4cf5e66a7413b4edee15
Simon Lee <simon@leefamily.uk.net>
2019-02-07 20:21:21 +0000

    Update update_panel.sh
    Added Panel Update Insert question

--- ubuntu_node/update_panel.sh
+++ ubuntu_node/update_panel.sh
@@ -23,26 +23,37 @@ echo "#############################################"
 
 # Change to Pterodactyl Web Directory
 cd /var/www/pterodactyl
+echo ""
 
 # Place Panel into Mainenace mode and Block Active Logins
 php artisan down
 
 # Download Latest Panel
-curl -L https://github.com/pterodactyl/panel/releases/download/v0.7.12/panel.tar.gz | tar --strip-components=1 -xzv
-chmod -R 755 storage/* bootstrap/cache
+echo ""
+echo " So your are wanting to update your panel??"
+echo " Please Visit: https://github.com/pterodactyl/panel/releases/"
+echo " Copy the link for the panel.tar.gz and paste below!"
+echo ""
 
-# Update Dependencies 
+read -p "Paste Here: " PanelRepo
+curl -L $PanelRepo | tar --strip-components=1 -xzv
+chmod -R 755 storage/* bootstrap/cache
+echo ""
+
+# Update Dependencies
 composer install --no-dev --optimize-autoloader
+echo ""
 
 # Reset Complied Template Cache
 php artisan view:clear
+echo ""
 
-# Update Database 
+# Update Database
 php artisan migrate --force
 php artisan db:seed --force
 
 # Reset Permisisons for Web Directory # UBUNTU
-chown -R www-data:www-data * 
+chown -R www-data:www-data *
 
 # Restore Panel to Acitve Mode - Allows Log Authentication
 php artisan up
