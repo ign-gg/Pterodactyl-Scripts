acadf13d1c73861abc2d7a68af0ae8d99d2969fb
Simon Lee <simon@leefamily.uk.net>
2019-02-12 22:44:18 +0000

    Update setup_panel.sh

--- ubuntu_node/setup_panel.sh
+++ ubuntu_node/setup_panel.sh
@@ -51,8 +51,8 @@ curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash
 apt-get update
 apt-add-repository universe
 
- apt-get -y install php7.2 php7.2-cli php7.2-gd php7.2-mysql php7.2-pdo php7.2-mbstring \
-                   php7.2-tokenizer php7.2-bcmath php7.2-xml php7.2-fpm php7.2-curl \
+apt-get -y install php7.2 php7.2-cli php7.2-gd php7.2-mysql php7.2-pdo php7.2-mbstring \
+root                   php7.2-tokenizer php7.2-bcmath php7.2-xml php7.2-fpm php7.2-curl \
                    php7.2-zip mariadb-server mariadb-client nginx tar unzip git redis-server \
                    certbot expect composer wget
                    
